d8c7df2ef5f1dd539a00f9eab24c47377d454645
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-10-15 12:58:27 +0530

    basic starter codes & db configured

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { PORT } from './config/serverConfig.js';
+import {StatusCodes} from 'http-status-codes'
+import connectDB from './config/dbConfig.js';
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/ping',(req,res)=>{
+    return res.status(StatusCodes.OK).json({message:'pong'});
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+    connectDB();
+})
